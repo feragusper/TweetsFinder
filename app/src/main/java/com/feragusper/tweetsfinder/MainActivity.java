136ba9fb7209682d1ce78f170b49b17df764b3a6
Fernando Perez <fernando.perez@globallogic.com>
2015-11-08 19:28:15 -0300

    * First Commit

---
+++ app/src/main/java/com/feragusper/tweetsfinder/MainActivity.java
@@ -0,0 +1,17 @@
+package com.feragusper.tweetsfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author Fernando.Perez
+ * @since 1.0
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
